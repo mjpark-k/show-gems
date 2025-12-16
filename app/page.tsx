3f7cf7518600a8ec08df74e8bbc0a04b494b75f1
mjpark <pmj9498@gmail.com>
2025-12-16 23:36:05 +0900

    chore: init Next.js project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className='p-8'>
+      <h1 className='text-2xl font-bold'>ShowGems</h1>
+      <p className='text-gray-600 mt-2'>My life movies, picked by me.</p>
+    </main>
+  );
+}
