ab9d0e0c2855f8674f63d430a9514002942ad89e
mjpark <pmj9498@gmail.com>
2025-12-17 23:13:27 +0900

    feat: setup main page and layout
    기본적인 레이아웃 설정 및 메인 페이지 나중에 수정하기

---
+++ app/ranking/page.tsx
@@ -0,0 +1,7 @@
+export default function Ranking() {
+  return (
+    <div>
+      <p>인생 영화 랭킹 및 검색 페이지</p>
+    </div>
+  );
+}
---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+  return (
+    <div>
+      <p>Sign In 화면</p>
+      <p>회원가입으로 가기</p>
+    </div>
+  );
+}
